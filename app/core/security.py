7654fc5fcbdb9ceb658a3eb486bb3ad7eb1005b8
Kulunchick <nikolikrasovski@gmail.com>
2023-05-16 21:16:53 +0300

    UPD: Added auth guard

---
+++ app/core/security.py
@@ -0,0 +1,87 @@
+from datetime import datetime
+from datetime import timedelta
+from typing import Union
+from uuid import UUID
+
+from authlib.jose import jwt
+from authlib.jose.errors import DecodeError
+from fastapi import Request, HTTPException, Depends
+from fastapi.security import HTTPBearer
+from pydantic import BaseModel, validator
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.config import settings
+from app.database import get_session
+from app.models import User
+from app.repositories.user import UserRepository
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 7 * 60 * 60  # 7 days
+ALGORITHM = "HS256"
+
+
+class TokenData(BaseModel):
+    sub: Union[UUID, str]
+    exp: datetime
+
+    @validator("sub")
+    def validate_uuids(cls, value):
+        if value:
+            return str(value)
+        return value
+
+
+class JWTRepo:
+    header = {
+        "alg": ALGORITHM
+    }
+
+    @classmethod
+    def create_access_token(cls, user_id: UUID) -> bytes:
+        payload = TokenData(
+            sub=user_id,
+            exp=datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        )
+        return jwt.encode(cls.header, payload.dict(), settings.SECRET_KEY.get_secret_value())
+
+    @classmethod
+    def decode_jwt_token(cls, token: str):
+        return jwt.decode(token, settings.SECRET_KEY.get_secret_value())
+
+
+class JWTBearer(HTTPBearer):
+    def __init__(self, auto_error: bool = True):
+        super(JWTBearer, self).__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request):
+        credentials = await super(JWTBearer, self).__call__(request)
+        if credentials:
+            if not credentials.scheme == "Bearer":
+                raise HTTPException(status_code=403, detail="Invalid authentication scheme.")
+            if not self.verify_jwt(credentials.credentials):
+                raise HTTPException(status_code=401, detail="Invalid token or expired token.")
+            return credentials.credentials
+        else:
+            raise HTTPException(status_code=403, detail="Invalid authorization code.")
+
+    @staticmethod
+    def verify_jwt(jwt_token: str) -> bool:
+        is_token_valid: bool = False
+
+        try:
+            payload = JWTRepo.decode_jwt_token(jwt_token)
+        except DecodeError:
+            payload = None
+        if payload:
+            is_token_valid = True
+        return is_token_valid
+
+
+async def get_user(token: str = Depends(JWTBearer()), session: AsyncSession = Depends(get_session)) -> User:
+    user_repository = UserRepository(session)
+    data = TokenData.parse_obj(JWTRepo.decode_jwt_token(token))
+
+    user = await user_repository.get_by_id(UUID(data.sub))
+    if user is None:
+        raise HTTPException(status_code=401, detail="Not authenticated")
+
+    return user
